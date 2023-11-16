2a7be9bcda3e395d524d45154abd66195d2fa521
Ntsanii <ntsane01m@gmail.com>
2023-11-15 16:49:50 -0800

    Simple Shell

--- getLine.c
+++ getLine.c
@@ -10,36 +10,36 @@
  */
 ssize_t input_buf(info_t *info, char **buf, size_t *len)
 {
-        ssize_t r = 0;
-        size_t len_p = 0;
+	ssize_t r = 0;
+	size_t len_p = 0;
 
-        if (!*len)
-        {
-                free(*buf);
-                *buf = NULL;
-                signal(SIGINT, sigintHandler);
+	if (!*len)
+	{
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigintHandler);
 #if USE_GETLINE
-                r = getline(buf, &len_p, stdin);
+		r = getline(buf, &len_p, stdin);
 #else
-                r = _getline(info, buf, &len_p);
+		r = _getline(info, buf, &len_p);
 #endif
-                if (r > 0)
-                {
-                        if ((*buf)[r - 1] == '\n')
-                        {
-                                (*buf)[r - 1] = '\0';
-                                r--;
-                        }
-                        info->linecount_flag = 1;
-                        remove_comments(*buf);
-                        build_history_list(info, *buf, info->histcount++);
-                        {
-                                *len = r;
-                                info->cmd_buf = buf;
-                        }
-                }
-        }
-        return (r);
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0';
+				r--;
+			}
+			info->linecount_flag = 1;
+			remove_comments(*buf);
+			build_history_list(info, *buf, info->histcount++);
+			{
+				*len = r;
+				info->cmd_buf = buf;
+			}
+		}
+	}
+	return (r);
 }
 
 /**
@@ -50,41 +50,41 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
  */
 ssize_t get_input(info_t *info)
 {
-        static char *buf;
-        static size_t a, j, len;
-        ssize_t r = 0;
-        char **buf_p = &(info->arg), *p;
+	static char *buf;
+	static size_t a, j, len;
+	ssize_t r = 0;
+	char **buf_p = &(info->arg), *p;
 
-        _putchar(BUF_FLUSH);
-        r = input_buf(info, &buf, &len);
-        if (r == -1)
-                return (-1);
-        if (len)
-        {
-                j = a;
-                p = buf + a;
+	_putchar(BUF_FLUSH);
+	r = input_buf(info, &buf, &len);
+	if (r == -1)
+		return (-1);
+	if (len)
+	{
+		j = a;
+		p = buf + a;
 
-                check_chain(info, buf, &j, a, len);
-                while (j < len)
-                {
-                        if (is_chain(info, buf, &j))
-                                break;
-                        j++;
-                }
+		check_chain(info, buf, &j, a, len);
+		while (j < len)
+		{
+			if (is_chain(info, buf, &j))
+				break;
+			j++;
+		}
 
-                a = j + 1;
-                if (a >= len)
-                {
-                        a = len = 0;
-                        info->cmd_buf_type = CMD_NORM;
-                }
+		a = j + 1;
+		if (a >= len)
+		{
+			a = len = 0;
+			info->cmd_buf_type = CMD_NORM;
+		}
 
-                *buf_p = p;
-                return (_strlen(p));
-        }
+		*buf_p = p;
+		return (_strlen(p));
+	}
 
-        *buf_p = buf;
-        return (r);
+	*buf_p = buf;
+	return (r);
 }
 
 /**
@@ -96,14 +96,14 @@ ssize_t get_input(info_t *info)
  */
 ssize_t read_buf(info_t *info, char *buf, size_t *i)
 {
-        ssize_t r = 0;
+	ssize_t r = 0;
 
-        if (*i)
-                return (0);
-        r = read(info->readfd, buf, READ_BUF_SIZE);
-        if (r >= 0)
-                *i = r;
-        return (r);
+	if (*i)
+		return (0);
+	r = read(info->readfd, buf, READ_BUF_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
 }
 
 /**
@@ -115,41 +115,41 @@ ssize_t read_buf(info_t *info, char *buf, size_t *i)
  */
 int _getline(info_t *info, char **ptr, size_t *length)
 {
-        static char buf[READ_BUF_SIZE];
-        static size_t a, len;
-        size_t k;
-        ssize_t r = 0, s = 0;
-        char *p = NULL, *new_p = NULL, *c;
+	static char buf[READ_BUF_SIZE];
+	static size_t a, len;
+	size_t k;
+	ssize_t r = 0, s = 0;
+	char *p = NULL, *new_p = NULL, *c;
 
-        p = *ptr;
-        if (p && length)
-                s = *length;
-        if (a == len)
-                a = len = 0;
+	p = *ptr;
+	if (p && length)
+		s = *length;
+	if (a == len)
+		a = len = 0;
 
-        r = read_buf(info, buf, &len);
-        if (r == -1 || (r == 0 && len == 0))
-                return (-1);
+	r = read_buf(info, buf, &len);
+	if (r == -1 || (r == 0 && len == 0))
+		return (-1);
 
-        c = _strchr(buf + a, '\n');
-        k = c ? 1 + (unsigned int)(c - buf) : len;
-        new_p = _realloc(p, s, s ? s + k : k + 1);
-        if (!new_p)
-                return (p ? free(p), -1 : -1);
+	c = _strchr(buf + a, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = _realloc(p, s, s ? s + k : k + 1);
+	if (!new_p)
+		return (p ? free(p), -1 : -1);
 
-        if (s)
-                _strncat(new_p, buf + a, k - a);
-        else
-                _strncpy(new_p, buf + a, k - a + 1);
+	if (s)
+		_strncat(new_p, buf + a, k - a);
+	else
+		_strncpy(new_p, buf + a, k - a + 1);
 
-        s += k - a;
-        a = k;
-        p = new_p;
+	s += k - a;
+	a = k;
+	p = new_p;
 
-        if (length)
-                *length = s;
-        *ptr = p;
-        return (s);
+	if (length)
+		*length = s;
+	*ptr = p;
+	return (s);
 }
 
 /**
@@ -160,7 +160,7 @@ int _getline(info_t *info, char **ptr, size_t *length)
  */
 void sigintHandler(__attribute__((unused))int sig_num)
 {
-        _puts("\n");
-        _puts("$ ");
-        _putchar(BUF_FLUSH);
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
 }
